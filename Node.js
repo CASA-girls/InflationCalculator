eb0453b56ec7cb440145133cd55142698b541d1e
Xueli Jiao <146323702+pumpkins628@users.noreply.github.com>
2023-11-05 01:24:12 +0000

    version 1.0

---
+++ Node.js
@@ -0,0 +1,40 @@
+function calculateInflation() {
+    const amount = parseFloat(document.getElementById("amountInput").value);
+    const yearSelect = document.getElementById("yearSelect").value;
+    const targetyearSelect = document.getElementById("targetyearSelect").value;
+    const userCountries = ['France', 'Italy', 'UK', 'Germany'];
+    const results = [];
+    const mysql = require('mysql');
+
+    // 创建数据库连接
+    const connection = mysql.createConnection({
+        host: 'casa0017.cetools.org',
+        user: 'ucqbutk',
+        password: 'zahugufelu',
+        database: 'ucqbutk'
+    });
+
+    // 连接到数据库
+    connection.connect();
+
+    // 执行SQL查询
+    const sqlQuery = `SELECT multiplier FROM inflationcalculator WHERE year = '${yearSelect}' AND targetyear = '${targetyearSelect}'`;
+    connection.query(sqlQuery, (error, results) => {
+        if (error) {
+            console.error('查询出错:', error);
+        } else {
+            // 处理查询结果
+            if (results.length === 2) {
+                const multiplier1 = results[0].multiplier;
+                const multiplier2 = results[1].multiplier;
+                const result = amount / multiplier1 * cmultiplier2;
+                console.log(`计算结果: ${result}`);
+            } else {
+                console.log('未找到匹配的数据。');
+            }
+        }
+    });
+
+    // 关闭数据库连接
+    connection.end();
+}
