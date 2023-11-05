d32228899552f6c4cbc4f1946a1d477edad81c88
ucfninf <ucfninf@ucl.ac.uk>
2023-11-05 02:12:02 +0000

    update graph sql code

--- Graph js/myEchartsScript.js
+++ Graph js/myEchartsScript.js
@@ -1,57 +1,106 @@
-// initialize echarts
-var myChart = echarts.init(document.querySelector('#Graph1'));
 
-// configure
-var option = {
-    legend: {
-        orient: 'vertical',
-        right: 10,
-        top: 'center'
-    },
-    // data
-    dataset: {
-        source: [
-            ['Country', 'France', 'UK', 'Italy', 'Germany'],
-            ['1960', 4.1399, 5, 2, 1, 8] 
+// The next step is to use the SQL into the echarts
 
-        ]
-    },
-    xAxis: { type: 'category' },
-    yAxis: {},
-    series: [
-        { type: 'bar' },
-        { type: 'bar' },
-        { type: 'bar' },
-        { type: 'bar' }
-    ]
-};
+const echarts = require('echarts');
+const mysql = require ('mysql')
 
-// Configure the chart
-myChart.setOption(option);
+// Connect to MySQL database
+const connection = mysql.createConnection({
+    host: 'casa0017.cetools.org',
+    user: 'ucqbutk',
+    password: 'zahugufelu',
+    database: 'inflationcalculator'
 
-// 
-const express = require('express');
-const mysql = require('mysql'); // Using the SQL data base on echart
-const app = express();
-
-// Connected to SQL
-//*const connection = mysql.createConnection({
-// host: casa0017.cetools.org,
-//  user: ucqbutk,
-//  password: zahugufelu,
-//  database: inflationcalculator
-//});]
-
-// import the SQL to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error('Error connecting to database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to database as id ' + connection.threadId);
 });
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:' + err.stack);
+      return;
+    }
+    console.log('Connected to database');
+});
+    // define sql query to retrieve data pur website need
+  app.get('/echartsData', (req,res) => {
+    const sqlQuery = 'SELECT year, country FROM inflationcalculator'; //This part is to edit and indicate our groups data table
+    //Execute the SQL query to get data
+    connection.query(sqlQuery, (error, results) => {
+      if (error) {
+        console.error('Error excuting query:' + error);
+        res.status(500).send('Error retrieving data from database')
+      } else {
+        res.send(results);
+      }
+    });
+  });
+    app.listen(3000,( =>{
+      console.log('server is running on port 3000');
+  }) ;
+   
+    
+
+    //Porcess the retrieved data to fit Echarts requirements
+      const processedData = processDataForEcharts(results);
+    
+    //-----------------------------Create charts-----------------------------------------------------------------------------------------------------------------------
+    fetch('/echartsData')
+
+    // initialize echarts
+    var myChart = echarts.init(document.querySelector('#Graph1'));
+      .then(response => response.json())
+      .then(data => {
+    
+    // Configure the chart by using our data
+    var option = {
+      legend: {
+          orient: 'vertical',
+          right: 10,
+          top: 'center'
+      },
+      // data
+      dataset: {
+          source: processedData
+           [
+              ['Country', 'France', 'UK', 'Italy', 'Germany'],
+              ['1960', 4.1399, 5, 2, 1, 8] 
+              
+          ]
+      },
+      xAxis: {
+         type: 'category',
+         data: processedData.categories
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+          { 
+            data:processedData.values,
+            type: 'bar'
+          }
+      ]
+    };
+ 
+//set the option to the chart
+myChart.setOption(option);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+//function to process the retrieved data for Echarts
+function processDataForEcharts(data) {
+  //Process the data from the database for Echarts here
+  return{
+    categories:data.map(item => item.year),
+    values:data.map(item => item.country)
+  };
+}
+
+//close the database connection after querying
+//connection.end();
+
 
 // API Endpoint to get data from MySQL
 // app.get('/data-from-mysql', function (req, res) {
-  const sqlQuery = year; // array on sql 
-
+// array on sql 
