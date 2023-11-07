43130f65966a1ce8e38b0a923223d4fa704113f2
ucfninf <ucfninf@ucl.ac.uk>
2023-11-07 22:19:11 +0000

    Change the first graph's data to graph value

--- Graph js/mysql.js
+++ Graph js/mysql.js
@@ -10,7 +10,7 @@ let dbDevMsqlConfig;
 dbDevMsqlConfig ={
     host     : 'casa0017.cetools.org', // ip 
     user     : 'ucqbutk', //username
-    password : '*******', //Passcode
+    password : 'zahugufelu', //Passcode
     port     :  '3306', //port
     database :  'ucqbutk' //Database
   };
