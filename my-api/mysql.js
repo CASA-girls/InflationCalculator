651bacf5a0d2ee9503b351d4302f5fa3313a9d8b
Xueli Jiao <146323702+pumpkins628@users.noreply.github.com>
2023-11-10 15:32:50 +0000

    calculator api section

---
+++ my-api/mysql.js
@@ -0,0 +1,15 @@
+//Information about the database needs to be hidden when uploading to Github
+
+let dbDevMsqlConfig;
+
+dbDevMsqlConfig = {
+    host: 'casa0017.cetools.org',
+    user: 'ucqbutk',
+    password: 'zahugufelu',
+    port: '3306',
+    database: 'ucqbutk'
+};
+
+module.exports = {
+    dbDevMsqlConfig
+}
