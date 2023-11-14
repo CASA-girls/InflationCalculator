77fb1a6a157b8cfdcde126565a7dd09e2ce92a7d
ucfninf <ucfninf@ucl.ac.uk>
2023-11-14 00:33:34 +0000

    gitignore

--- back_end/mysql.js
+++ back_end/mysql.js
@@ -10,7 +10,7 @@ let dbDevMsqlConfig;
 dbDevMsqlConfig = {
   host: 'casa0017.cetools.org', // ip 
   user: 'ucqbutk', //username
-  password: "zahugufelu", // Set environment variable
+  password: process.env.PASSWORD, // Set environment variable
   port: '3306', //port
   database: 'ucqbutk' //Database
 };
