81fe5d3e036d940bb92fb07c2331be8e9ad4f212
ucfninf <ucfninf@ucl.ac.uk>
2023-11-14 23:10:04 +0000

    minor changes

--- back_end/mysql.js
+++ back_end/mysql.js
@@ -2,10 +2,6 @@ let dbDevMsqlConfig;
 
 //this commend is use for develop a envi
 
-// find and solve error from mysql and node js mysql Referenced from:
-//https://blog.csdn.net/m0_59962820/article/details/129389947
-
-
 //Configure SQL database
 dbDevMsqlConfig = {
   host: 'casa0017.cetools.org', // ip 
